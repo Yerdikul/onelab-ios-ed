5a43329532ac084383872a05bfb02099caf2d475
KizatovArman <kizatov.armashka.05@gmail.com>
2024-03-05 18:06:25 +0500

    [homework1] stack complete

--- l1/homeworks/KizatovArman.swift
+++ l1/homeworks/KizatovArman.swift
@@ -6,8 +6,28 @@ class Stack<T> {
     func push(_ item: T) {
         items.append(item)
     }
+
+    func isEmpty() -> Bool {
+        items.isEmpty
+    }
     
-    func printStack() {
-        print(items)
+    func size() -> Int {
+        items.count
+    }
+    
+    func peek() -> T? {
+        items.isEmpty ? nil : items.last
+    }
+    
+    func pop() -> T? {
+        items.isEmpty ? nil : items.removeLast()
     }
 }
+
+var stack = Stack<Int>()
+stack.push(1)
+stack.push(2)
+print(stack.peek() ?? "Стек пуст") // Должно напечатать "2" -> True
+print(stack.pop() ?? "Стек пуст")   // Должно напечатать "2" -> True
+print(stack.pop() ?? "Стек пуст")   // Должно напечатать "1" -> True
+print(stack.pop() ?? "Стек пуст")   // Должно напечатать "Стек пуст" -> True
