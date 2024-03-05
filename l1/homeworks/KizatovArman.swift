3d424acbe17c8cef61508cfdf29bded88a6cee0c
KizatovArman <kizatov.armashka.05@gmail.com>
2024-03-05 18:12:57 +0500

    [homework1] queue complete

--- l1/homeworks/KizatovArman.swift
+++ l1/homeworks/KizatovArman.swift
@@ -31,3 +31,35 @@ print(stack.peek() ?? "Стек пуст") // Должно напечатать 
 print(stack.pop() ?? "Стек пуст")   // Должно напечатать "2" -> True
 print(stack.pop() ?? "Стек пуст")   // Должно напечатать "1" -> True
 print(stack.pop() ?? "Стек пуст")   // Должно напечатать "Стек пуст" -> True
+
+class Queue<T> {
+    private var items: [T] = []
+    
+    func enqueue(_ item: T) {
+        items.append(item)
+    }
+
+    func isEmpty() -> Bool {
+        items.isEmpty
+    }
+    
+    func size() -> Int {
+        items.count
+    }
+    
+    func peek() -> T? {
+        items.isEmpty ? nil : items.first
+    }
+    
+    func dequeue() -> T? {
+        items.isEmpty ? nil : items.removeFirst()
+    }
+}
+
+var queue = Queue<String>()
+queue.enqueue("Первый")
+queue.enqueue("Второй")
+print(queue.peek() ?? "Очередь пуста") // Должно напечатать "Первый" -> True
+print(queue.dequeue() ?? "Очередь пуста") // Должно напечатать "Первый" -> True
+print(queue.dequeue() ?? "Очередь пуста") // Должно напечатать "Второй" -> True
+print(queue.dequeue() ?? "Очередь пуста") // Должно напечатать "Очередь пуста" -> True
