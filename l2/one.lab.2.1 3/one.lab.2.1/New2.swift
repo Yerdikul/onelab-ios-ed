2f72d32bb95fe0cf1b10eed00ac5bc703956bd6e
Adil Bazylov <adil.bazylov@gmail.com>
2024-03-24 15:06:27 +0500

    [HW2] delete git file to unarchive properly

---
+++ l2/one.lab.2.1 3/one.lab.2.1/New2.swift
@@ -0,0 +1,9 @@
+//
+//  New2.swift
+//  one.lab.2.1
+//
+//  Created by Nurbolat Yerdikul on 02.03.2024.
+//
+
+import Foundation
+    
---
+++ l2/one.lab.2.1 3/one.lab.2.1/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  one.lab.2.1
+//
+//  Created by Nurbolat Yerdikul on 02.03.2024.
+//
+// disable
+import UIKit
+import Alamofire
+
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        print("String")
+        // Do any additional setup after loading the view.
+    }
+}
